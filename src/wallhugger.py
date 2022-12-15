d217ceb1e6b8cb4a8cc85bea035c426004df6f5f
toferwon <68505494+toferwon@users.noreply.github.com>
2022-12-15 09:49:32 -0800

    Update wallhugger.py

--- src/wallhugger.py
+++ src/wallhugger.py
@@ -31,31 +31,11 @@ def clbk_laser(msg):
       if(translated[i] == 0):
         translated[i] = 10;
     regions_ = {
-#        'right':  min(min(msg.ranges[210:270]), 10),
-#        'fright': min(min(msg.ranges[271:330]), 10),
-#        'front':  min(min(msg.ranges[331:359]),min(msg.ranges[0:30]), 10),
-#        'fleft':  min(min(msg.ranges[31:90]), 10),
-#        'left':   min(min(msg.ranges[91:150]), 10),
-
-#        'right':  min(min(translated[210:270]), 10),
-#        'fright': min(min(translated[271:330]), 10),
-#        'front':  min(min(translated[331:359]),min(translated[0:30]), 10),
-#        'fleft':  min(min(translated[31:90]), 10),
-#        'left':   min(min(translated[91:150]), 10),
-
         'right':  min(min(translated[271:306]), 10),
         'fright': min(min(translated[307:342]), 10),
         'front':  min(min(translated[343:359]),min(translated[0:18]), 10),
         'fleft':  min(min(translated[19:54]), 10),
         'left':   min(min(translated[55:90]), 10),
-
-
-#        'left':  min(min(translated[210:270]), 10),
-#        'fleft': min(min(translated[271:350]), 10),
-#        'front':  min(min(translated[351:359]),min(translated[0:10]), 10),
-#        'fright':  min(min(translated[11:90]), 10),
-#        'right':   min(min(translated[91:150]), 10),
-
     }
     print regions_
     
